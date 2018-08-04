fc03673c2f7b4d269ceb76cb6148f62bc1b09d90
Nolan Hawkins <nhawkins@paytronix.com>
2018-08-04 10:19:34 -0400

    so close

--- hbtest1.rb
+++ hbtest1.rb
@@ -1,7 +1,7 @@
 class Hbtest1 < Formula
   desc "This is a home rbew test formula"
   homepage "https://github.com/nhawkin/homebrew-test"
-  url "https://github.com/nhawkin/homebrew-test/releases/download/v0.2/test1-0.3.tar.gz"
+  url "https://github.com/nhawkin/homebrew-test/releases/download/v0.3/test1-0.3.tar.gz"
   sha256 "b8c9a65dbdc98756b96a7abf787382f642697e13f3fe5a1e24078bb042f7546b"
   version "0.3"
   
